4db4298945ee1f19676b3b3261021418778970f3
MuhammadTayyab88 <105416506+MuhammadTayyab88@users.noreply.github.com>
2024-12-03 21:35:56 +0500

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -17,7 +17,7 @@ STRIPE_SECRET_KEY=config('STRIPE_SECRET_KEY', default='')
 STRIPE_PUBLISHABLE_KEY =config('STRIPE_PUBLISHABLE_KEY', default='')
 DEBUG = config('DEBUG', default=False)
 
-ALLOWED_HOSTS = ["3.126.55.131",'http://iqcollections.store/']
+ALLOWED_HOSTS = ["iqcollections.store"]
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
